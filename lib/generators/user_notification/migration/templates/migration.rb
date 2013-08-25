c394b8dbec963cb604e4cbbcdc61d0cff1fe8fda
Meck <yesmeck@gmail.com>
2013-08-26 04:06:44 +0800

    Typo

--- lib/generators/user_notification/migration/templates/migration.rb
+++ lib/generators/user_notification/migration/templates/migration.rb
@@ -8,7 +8,7 @@ class CreateNotifications < ActiveRecord::Migration
       t.string  :key
       t.text    :parameters
       t.belongs_to :recipient, :polymorphic => true
-      t.boolwan :read, default: false
+      t.boolean :read, default: false
 
       t.timestamps
     end
