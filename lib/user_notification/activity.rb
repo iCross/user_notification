48398225669e64b4e5aae44cdc55a8f300432912
Meck <yesmeck@gmail.com>
2013-08-26 21:27:40 +0800

    Remove unused file

--- lib/user_notification/activity.rb
+++
@@ -1,6 +0,0 @@
-module UserNotification
-  # Main model, stores all information about what happened,
-  # who caused it, when and anything else.
-  class Notification < inherit_orm("Notification")
-  end
-end
