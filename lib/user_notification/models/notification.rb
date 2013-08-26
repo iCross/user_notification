c24b3c1400b57d360662de2928efe1795a2e8789
Meck <yesmeck@gmail.com>
2013-08-26 22:58:45 +0800

    fix mark_as_read_for!

--- lib/user_notification/models/notification.rb
+++ lib/user_notification/models/notification.rb
@@ -2,7 +2,7 @@ module UserNotification
   class Notification < inherit_orm("Notification")
 
     def mark_as_read_for!(recipient)
-      self.notifyings.where(recipient: recipient).update_all(:read, true)
+      self.notifyings.where(recipient: recipient).update_all(read: true)
     end
 
     def read_by?(recipient)
